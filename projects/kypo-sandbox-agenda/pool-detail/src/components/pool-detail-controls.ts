d238f23149dbd7f585a47bbc62b095355a6712ae
Lukáš Majdan <445519@mail.muni.cz>
2022-09-29 15:29:43 +0200

    Resolve "In the Pool Detail page, add "Delete All Unlocked" button"

--- projects/kypo-sandbox-agenda/pool-detail/src/components/pool-detail-controls.ts
+++ projects/kypo-sandbox-agenda/pool-detail/src/components/pool-detail-controls.ts
@@ -1,4 +1,8 @@
-import { SentinelControlItem } from '@sentinel/components/controls';
+import {
+  SentinelControlItem,
+  SentinelControlMenuItem,
+  SentinelExpandableControlItem,
+} from '@sentinel/components/controls';
 import { Pool } from '@muni-kypo-crp/sandbox-model';
 import { defer, of } from 'rxjs';
 import { AbstractSandbox } from '../model/abstract-sandbox';
@@ -9,8 +13,10 @@ import { SandboxInstanceService } from '../services/state/sandbox-instance/sandb
  */
 export class PoolDetailControls {
   static readonly ALLOCATE_ACTION_ID = 'allocate';
+  static readonly DELETE_ACTION_ID = 'delete_menu';
   static readonly DELETE_FAILED_ACTION_ID = 'delete_failed';
   static readonly DELETE_ALL_ACTION_ID = 'delete_all';
+  static readonly DELETE_UNLOCKED_ACTION_ID = 'delete_unlocked';
 
   static create(
     pool: Pool,
@@ -25,25 +31,47 @@ export class PoolDetailControls {
         of(pool.maxSize === sandboxes.length),
         defer(() => sandboxInstanceService.allocate(pool.id))
       ),
-      new SentinelControlItem(
-        this.DELETE_FAILED_ACTION_ID,
-        'Delete Failed',
-        'warn',
-        of(PoolDetailControls.someSandboxFailedAndHasNoRunningCleanup(sandboxes)),
-        defer(() => sandboxInstanceService.cleanupFailed(pool.id, true))
-      ),
-      new SentinelControlItem(
-        this.DELETE_ALL_ACTION_ID,
-        'Delete All',
+      new SentinelExpandableControlItem(
+        this.DELETE_ACTION_ID,
+        'Delete',
         'warn',
         of(PoolDetailControls.someSandboxHasNoRunningCleanup(sandboxes)),
-        defer(() => sandboxInstanceService.cleanupMultiple(pool.id, true))
+        [
+          new SentinelControlMenuItem(
+            this.DELETE_ALL_ACTION_ID,
+            'Delete All',
+            'warn',
+            of(PoolDetailControls.someSandboxHasNoRunningCleanup(sandboxes)),
+            defer(() => sandboxInstanceService.cleanupMultiple(pool.id, true)),
+            'delete'
+          ),
+          new SentinelControlMenuItem(
+            this.DELETE_FAILED_ACTION_ID,
+            'Delete Failed',
+            'warn',
+            of(PoolDetailControls.someSandboxFailedAndHasNoRunningCleanup(sandboxes)),
+            defer(() => sandboxInstanceService.cleanupFailed(pool.id, true)),
+            'delete_forever'
+          ),
+          new SentinelControlMenuItem(
+            this.DELETE_UNLOCKED_ACTION_ID,
+            'Delete Unlocked',
+            'warn',
+            of(PoolDetailControls.someSandboxUnlockedAndHasNoRunningCleanup(sandboxes)),
+            defer(() => sandboxInstanceService.cleanupUnlocked(pool.id, true)),
+            'delete_outline'
+          ),
+        ]
       ),
     ];
   }
 
   private static someSandboxFailedAndHasNoRunningCleanup(sandboxes: AbstractSandbox[]) {
     return !sandboxes.some((sandbox) => sandbox.allocationFailed() && !sandbox.cleanupRunning());
+  }
+
+  private static someSandboxUnlockedAndHasNoRunningCleanup(sandboxes: AbstractSandbox[]) {
+    return !sandboxes.some((sandbox: AbstractSandbox) => !sandbox.locked && !sandbox.cleanupRunning());
   }
 
   private static someSandboxHasNoRunningCleanup(sandboxes: AbstractSandbox[]) {
