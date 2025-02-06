953d27fd398809b82af4be8e1d8d5cb138b256ab
Mário Murín <murin@cshub.cz>
2025-02-06 11:27:41 +0100

    Minor refactoring

--- projects/kypo-sandbox-agenda/pool-overview/src/components/pool-comment/pool-comment.component.ts
+++ projects/kypo-sandbox-agenda/pool-overview/src/components/pool-comment/pool-comment.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { PoolCommentFormGroup } from './pool-comment-form-group';
 import { PoolOverviewService } from '../../services/state/pool-overview/pool-overview.service';
 import { Pool } from '@muni-kypo-crp/sandbox-model';
@@ -17,12 +17,12 @@ export class PoolCommentComponent implements OnInit, OnChanges {
   constructor(private poolOverviewService: PoolOverviewService) {}
 
   ngOnInit() {
-    if(!this.commentFormGroup){
+    if (!this.commentFormGroup) {
       this.initFormGroup();
     }
   }
 
-  private initFormGroup(){
+  private initFormGroup() {
     this.commentFormGroup = new PoolCommentFormGroup(this.element.comment);
   }
 
@@ -35,14 +35,14 @@ export class PoolCommentComponent implements OnInit, OnChanges {
   }
 
   onInput() {
-     this.commentFormGroup.setValuesToPool(this.element);
-     this.poolOverviewService.updateComment(this.element).subscribe();
+    this.commentFormGroup.setValuesToPool(this.element);
+    this.poolOverviewService.updateComment(this.element).subscribe();
   }
 
-  ngOnChanges(changes:SimpleChanges){
-    if(!this.commentFormGroup) {
+  ngOnChanges() {
+    if (!this.commentFormGroup) {
       this.initFormGroup();
-    } else{
+    } else {
       this.commentFormGroup.formGroup.get('comment').setValue(this.element.comment);
     }
   }
